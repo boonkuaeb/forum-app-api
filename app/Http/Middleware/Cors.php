66d61f4ee82a72f42e3d8257fb37679b3b9f3a2a
boonkuae_boo <john.kuae@gmail.com>
2016-06-21 14:00:59 +0700

    15-CORS header middleware

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        return $next($request)
+                ->header('Access-Control-Allow-Origin','http://192.168.99.100:80')
+                ->header('Access-Control-Allow-Methods','GET','POST','OPTIONS')
+                ->header('Access-Control-Allow-Headers','X-Requested-With, Content-Type, Authorization');
+    }
+}
